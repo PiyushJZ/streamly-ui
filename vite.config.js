41c4d742f86669de28a37fa0966183c4ef3dd8f3
Piyush Jain <piyushjz0402@gmail.com>
2025-07-03 01:38:58 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  preview: {
+    port: 3000,
+    host: true,
   },
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
@@ -17,11 +21,11 @@ export default defineConfig({
   ],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
